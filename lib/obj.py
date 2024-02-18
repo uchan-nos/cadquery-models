c8208c492ed63fecc4456738e281265977ae4963
uchan-nos <uchan@uchan.net>
2024-02-18 22:12:30 +0900

    板の曲げ加工にフィレットを追加

--- lib/obj.py
+++ lib/obj.py
@@ -4,15 +4,21 @@ Copyright (c) 2024 Kota UCHIDA
 汎用的な造形を担当する関数群
 '''
 
+from cadquery import NearestToPointSelector, Selector
 import math
 
-def bended_board_2d(workplane, way_points, board_t):
+class NullSelector(Selector):
+    def filter(self, objectList):
+        return []
+
+def bended_board_2d(workplane, way_points, board_t, sharpness=0.5):
     '''
     板を曲げ加工したような図形の横面図を生成
 
     :param workplane: 横面図を置く平面
     :param way_points: 板の中心点列
     :param board_t: 板厚
+    :param shapness: 曲げ加工の鋭さ（0～1）
     '''
 
     def make_both_side_points(wp, dir, t):
@@ -32,6 +38,8 @@ def bended_board_2d(workplane, way_points, board_t):
 
     side1_points = []
     side2_points = []
+    outer_points = []
+    inner_points = []
 
     # the first way point
     line_dir = calc_line_dir(lines[0])
@@ -50,16 +58,28 @@ def bended_board_2d(workplane, way_points, board_t):
         side1_points.append(side_points[0])
         side2_points.append(side_points[1])
 
+        if l1_dir < l0_dir:
+            outer_points.append(side_points[0])
+            inner_points.append(side_points[1])
+        elif l1_dir > l0_dir:
+            outer_points.append(side_points[1])
+            inner_points.append(side_points[0])
+
     # the last way point
     line_dir = calc_line_dir(lines[-1])
     side_points = make_both_side_points(way_points[-1], line_dir, board_t)
     side1_points.append(side_points[0])
     side2_points.append(side_points[1])
 
-    board2d = workplane.moveTo(*side1_points[0])
-    for p in side1_points[1:]:
-        board2d = board2d.lineTo(*p)
-    for p in reversed(side2_points):
-        board2d = board2d.lineTo(*p)
-    board2d = board2d.close()
-    return board2d
+    fillet_factor = (1 - sharpness) / 2
+    skt = workplane.sketch()
+    skt.polygon(side1_points + list(reversed(side2_points)))
+    if fillet_factor > 0:
+        skt.vertices(
+            sum(map(NearestToPointSelector, inner_points), start=NullSelector())
+        ).fillet(board_t*fillet_factor).reset()
+    skt.vertices(
+        sum(map(NearestToPointSelector, outer_points), start=NullSelector())
+    ).fillet(board_t*(fillet_factor+0.5)).reset()
+
+    return skt.finalize()
